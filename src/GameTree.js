b20395af63e44ddbe6d8916784e9e5fb07ed7301
Yichuan Shen <yichuan.shen@sap.com>
2018-11-13 16:34:28 +0100

    Add CRDT GameTree class with simple operations

--- src/GameTree.js
+++ src/GameTree.js
@@ -1,3 +1,62 @@
-module.exports = class GameTree {
+const EventEmitter = require('events')
+const {uuid, sha1, compareOperations} = require('./helper')
 
+module.exports = class GameTree extends EventEmitter {
+    constructor({id = null, base = {}} = {}) {
+        this.id = id == null ? uuid() : id
+        this.timestamp = 0
+
+        this.operations = []
+        this.base = Object.assign({
+            nodes: {},
+            parents: {},
+            children: {}
+        }, base)
+    }
+
+    applyOperation(operation) {
+        let {id, timestamp} = operation
+
+        if (this.operations.some(o => o.id === id)) return
+        if (timestamp > this.timestamp) this.timestamp = timestamp
+
+        let i = this.operations.length - 1
+
+        while (i >= 0) {
+            let op = this.operations[i]
+            let comparison = compareOperations(op, operation)
+            if (comparison <= 0) break
+
+            i--
+        }
+
+        this.operations.splice(i + 1, 0, operation)
+    }
+
+    pushOperation(type, payload) {
+        this.timestamp++
+
+        let operation = {
+            id: sha1(this.id, this.timestamp),
+            author: this.id,
+            timestamp: this.timestamp,
+            type,
+            payload
+        }
+
+        this.operations.push(operation)
+        this.emit('operation', operation)
+    }
+
+    appendNode(parent, data) {
+        this.pushOperation('appendNode', {
+            parent,
+            id: sha1(parent, this.id, this.timestamp),
+            data
+        })
+    }
+
+    removeNode(id) {
+        this.pushOperation('removeNode', {id})
+    }
 }
---
+++ src/helper.js
@@ -0,0 +1,10 @@
+const uuid = require('uuid/v4')
+const crypto = require('crypto')
+
+exports.uuid = () => uuid()
+exports.sha1 = (...x) => crypto.createHash('sha1').update(x.join('-')).digest('hex')
+exports.compare = (x, y) => x < y ? -1 : +(x === y)
+exports.compareOperations = (o1, o2) =>
+    compare(o1.timestamp, o2.timestamp)
+    || compare(o1.id, o2.id)
+    || compare(o1.author, o2.author)
