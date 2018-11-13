453541e941fa45dd4a22a588258f52bac332ec09
Yichuan Shen <shen.yichuan@gmail.com>
2018-11-13 21:24:42 +0100

    Add GameTree.get(Node|Parent|Children|Sequence)()

--- src/GameTree.js
+++ src/GameTree.js
@@ -8,8 +8,8 @@ module.exports = class GameTree extends EventEmitter {
 
         this.operations = []
         this.base = Object.assign({
-            nodes: {},
-            parents: {},
+            node: {},
+            parent: {},
             children: {}
         }, base)
     }
@@ -68,19 +68,89 @@ module.exports = class GameTree extends EventEmitter {
         this._pushOperation('removeFromProperty', {id, property, value})
     }
 
-    updateProperty(id, property, value) {
-        this._pushOperation('updateProperty', {id, property, value})
+    updateProperty(id, property, values) {
+        this._pushOperation('updateProperty', {id, property, values})
     }
 
-    removeProperty(id, property) {
-        this._pushOperation('updateProperty', {id, property, value: null})
+    getNode(id) {
+        let base = deepCopy(this.base.node[id])
+
+        for (let {type, payload} of this.operations) {
+            if (payload.id !== id) continue
+
+            if (type === 'appendNode') {
+                base = deepCopy(payload.data)
+            } else if (type === 'removeNode') {
+                return null
+            } else if (type === 'addToProperty' && base != null) {
+                if (payload.property in base) {
+                    if (!base[payload.property].includes(payload.value)) {
+                        base[payload.property].push(payload.value)
+                    }
+                } else {
+                    base[payload.property] = [payload.value]
+                }
+            } else if (type === 'removeFromProperty' && base != null) {
+                if (payload.property in base) {
+                    let index = base[payload.property].indexOf(payload.value)
+                    if (index >= 0) base[payload.property].splice(index, 1)
+                }
+            } else if (type === 'updateProperty' && base != null) {
+                if (payload.values != null) {
+                    base[payload.property] = [...payload.values]
+                } else {
+                    delete base[payload.property]
+                }
+            }
+        }
+
+        return base
     }
 
-    updateAttribute(id, attribute, value) {
-        this._pushOperation('updateAttribute', {id, attribute, value})
+    getParent(id) {
+        let base = this.base.parent[id]
+
+        for (let {type, payload} of this.operations) {
+            if (type === 'appendNode' && payload.id === id) {
+                base = payload.parent
+            } else if (type === 'removeNode' && (payload.id === id || payload.parent === id)) {
+                return null
+            }
+        }
+
+        return base
     }
 
-    removeAttribute(id, attribute) {
-        this._pushOperation('updateAttribute', {id, attribute, value: null})
+    getChildren(id) {
+        let base = [...this.base.children[id]] || []
+
+        for (let {type, payload} of this.operations) {
+            if (type === 'appendNode' && payload.parent === id) {
+                base.push(payload.id)
+            } else if (type === 'removeNode') {
+                if (payload.id === id) {
+                    return []
+                } else {
+                    let index = base.indexOf(id)
+                    if (index >= 0) base.splice(index, 1)
+                }
+            }
+        }
+
+        return base
+    }
+
+    getSequence(id) {
+        let node = this.getNode(id)
+        let nodes = [node]
+        let children = this.getChildren(id)
+
+        while (children.length === 1) {
+            let child = this.getNode(children[0])
+            nodes.push(child)
+            children = this.getChildren(children[0])
+        }
+
+        return {nodes, children}
     }
 }
--- src/helper.js
+++ src/helper.js
@@ -3,8 +3,15 @@ const crypto = require('crypto')
 
 exports.uuid = () => uuid()
 exports.sha1 = (...x) => crypto.createHash('sha1').update(x.join('-')).digest('hex')
+
 exports.compare = (x, y) => x < y ? -1 : +(x === y)
 exports.compareOperations = (o1, o2) =>
     compare(o1.timestamp, o2.timestamp)
     || compare(o1.id, o2.id)
     || compare(o1.author, o2.author)
+
+exports.deepClone = x =>
+    Array.isArray(x) ? x.map(y => exports.deepClone(y))
+    : x == null ? x
+    : typeof x !== 'object' ? x
+    : Object.keys(x).reduce((acc, k) => (acc[k] = exports.deepClone(x[k]), acc), {})
