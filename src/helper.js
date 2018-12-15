75bf3036d072b616cde170f245d9babf8ac4a02a
Yichuan Shen <shen.yichuan@gmail.com>
2018-12-15 22:02:33 +0100

    Start anew

--- src/helper.js
+++ src/helper.js
@@ -8,9 +8,3 @@ exports.compare = (x, y) => x < y ? -1 : +(x !== y)
 exports.compareOperations = (o1, o2) =>
     exports.compare(o1.timestamp, o2.timestamp)
     || exports.compare(o1.id, o2.id)
-
-exports.deepCopy = x =>
-    x == null ? x
-    : typeof x !== 'object' ? x
-    : Array.isArray(x) ? x.map(y => exports.deepCopy(y))
-    : Object.keys(x).reduce((acc, k) => (acc[k] = exports.deepCopy(x[k]), acc), {})
