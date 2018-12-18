99cd7134a7c7c4e4556e554dc17d2fcd2c2e3fc3
Yichuan Shen <shen.yichuan@gmail.com>
2018-12-18 20:45:57 +0100

    Add tests for tree.getHistory()

--- tests/crdt.test.js
+++ tests/crdt.test.js
@@ -8,6 +8,7 @@ t.test('getChanges', t => {
 
     let newTree = tree.mutate(draft => {
         draft.addToProperty(tree.root.id, 'MA', 'dd')
+        draft.updateProperty(tree.root.id, 'MA', ['dd', 'df'])
         draft.removeFromProperty(tree.root.id, 'MA', 'df')
     })
 
@@ -29,14 +30,91 @@ t.test('getChanges', t => {
             "args": [
                 tree.root.id,
                 "MA",
+                ["dd", "df"]
+            ],
+            "operation": "updateProperty",
+            "returnValue": true,
+            "timestamp": 1
+        },
+        {
+            "actorId": tree.id,
+            "args": [
+                tree.root.id,
+                "MA",
                 "df"
             ],
             "operation": "removeFromProperty",
             "returnValue": true,
-            "timestamp": 1
+            "timestamp": 2
         }
     ])
 
     t.end()
 })
 
+t.test('getHistory', t => {
+    let tree = new GameTree()
+
+    t.deepEqual(tree.getHistory(), [])
+
+    let newTree = tree.mutate(draft => {
+        draft.addToProperty(tree.root.id, 'MA', 'dd')
+    })
+
+    t.deepEqual(newTree.getHistory(), [
+        {
+            "actorId": tree.id,
+            "args": [
+                tree.root.id,
+                "MA",
+                "dd"
+            ],
+            "operation": "addToProperty",
+            "returnValue": true,
+            "timestamp": 0
+        }
+    ])
+
+    newTree = newTree.mutate(draft => {
+        draft.addToProperty(tree.root.id, 'MA', 'df')
+        draft.removeFromProperty(tree.root.id, 'MA', 'dd')
+    })
+
+    t.deepEqual(newTree.getHistory(), [
+        {
+            "actorId": tree.id,
+            "args": [
+                tree.root.id,
+                "MA",
+                "dd"
+            ],
+            "operation": "addToProperty",
+            "returnValue": true,
+            "timestamp": 0
+        },
+        {
+            "actorId": tree.id,
+            "args": [
+                tree.root.id,
+                "MA",
+                "df"
+            ],
+            "operation": "addToProperty",
+            "returnValue": true,
+            "timestamp": 1
+        },
+        {
+            "actorId": tree.id,
+            "args": [
+                tree.root.id,
+                "MA",
+                "dd"
+            ],
+            "operation": "removeFromProperty",
+            "returnValue": true,
+            "timestamp": 2
+        }
+    ])
+
+    t.end()
+})
