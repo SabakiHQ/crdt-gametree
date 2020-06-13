d518b8da170ee851a9676cf27b52d63241a6bbd1
Yichuan Shen <shen.yichuan@gmail.com>
2020-06-13 18:27:45 +0200

    Cleanup

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
-const uuid = require('uuid/v4')
+const {v4: uuid} = require('uuid')
 
 const uuidChars = '0123456789'
     + 'abcdefghijklmnopqrstuvwxyz'
@@ -30,9 +30,10 @@ exports.uuid = () => {
 }
 
 exports.compare = (x, y) => x < y ? -1 : +(x > y)
-exports.compareChange = (c1, c2) =>
-    exports.compare(c1.timestamp, c2.timestamp)
-    || exports.compare(c1.author, c2.author)
+
+exports.compareMap = function compareMap(fn, cmp = defaultCmp) {
+    return (x, y) => cmp(fn(x), fn(y))
+}
 
 exports.compareLexically = (compareFn = exports.compare) => (arr1, arr2) => {
     let inner = i => {
@@ -43,59 +44,4 @@ exports.compareLexically = (compareFn = exports.compare) => (arr1, arr2) => {
     }
 
     return inner(0)
-}
-
-exports.wrapProperties = (data, properties, fn) => {
-    return Object.keys(data).reduce((acc, property) => {
-        if (
-            properties.includes(property)
-            && data[property] != null
-            && data[property][0] != null
-        ) {
-            acc[property] = [fn(data[property][0])]
-        } else {
-            acc[property] = data[property]
-        }
-
-        return acc
-    }, {})
-}
-
-exports.diffString = (from, to, fromStart = 0, toStart = 0) => {
-    from = Array.from(from)
-    to = Array.from(to)
-
-    if (toStart >= to.length && fromStart >= from.length) {
-        return {deletions: [], insertions: []}
-    } else if (toStart >= to.length) {
-        return {
-            deletions: [...Array(from.length - fromStart)].map((_, i) => fromStart + i),
-            insertions: []
-        }
-    } else if (fromStart >= from.length) {
-        return {
-            deletions: [],
-            insertions: [{at: fromStart, insert: to.slice(toStart).join('')}],
-        }
-    } else if (from[fromStart] === to[toStart]) {
-        return exports.diffString(from, to, fromStart + 1, toStart + 1)
-    }
-
-    let deletionResult = exports.diffString(from, to, fromStart + 1, toStart)
-    let insertionResult = exports.diffString(from, to, fromStart, toStart + 1)
-    let complexity = changes => changes.deletions.length
-        + changes.insertions.reduce((sum, {insert}) => sum + insert.length, 0)
-
-    if (complexity(deletionResult) < complexity(insertionResult)) {
-        deletionResult.deletions.push(fromStart)
-        return deletionResult
-    } else {
-        if (insertionResult.insertions.length > 0 && insertionResult.insertions[0].at === fromStart) {
-            insertionResult.insertions[0].insert = to[toStart] + insertionResult.insertions[0].insert
-        } else {
-            insertionResult.insertions.unshift({at: fromStart, insert: to[toStart]})
-        }
-
-        return insertionResult
-    }
 }
