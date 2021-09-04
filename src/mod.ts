e0f995540349351a229a53549a90d7835e7ead21
Yichuan Shen <shen.yichuan@gmail.com>
2021-09-04 14:19:39 +0200

    Fix type export

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,7 @@
 export * from "./types.ts";
 export { GameTree } from "./GameTree.ts";
-export { Change, TimestampedChange } from "./Change.ts";
+export { Change } from "./Change.ts";
+export type { TimestampedChange } from "./Change.ts";
 export type { Mutator } from "./Mutator.ts";
 export type { FracPos } from "./fractionalPosition.ts";
 export type { Timestamped, TimestampedValue } from "./timestamp.ts";
